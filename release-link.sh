d324ee13eedcea12ef4de44dac23a2ded9ad232f
Dave DPI <dave@dpi.nl>
2020-07-22 11:14:40 +0200

    Remove symlink before making new one

--- release-link.sh
+++ release-link.sh
@@ -7,4 +7,5 @@ echo "Linking $releasename to $webroot"
 
 ln -s /var/www/media "/var/www/releases/$releasename/media"
 ln -s /var/www/common/env.php "/var/www/releases/$releasename/env.php"
+rm $webroot
 ln -s "/var/www/releases/$releasename" $webroot
