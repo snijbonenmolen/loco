514562cfceef0e00c17c19d51e3ea43a3ce65c7f
Dave DPI <dave@dpi.nl>
2020-07-22 08:41:12 +0200

    Optional media folder, preparations for dotenv

--- index.php
+++ index.php
@@ -1,40 +1,51 @@
+<html>
+<head>
+    <title>Test</title>
+</head>
+<body>
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-    var_dump('deze');
+$rq = new \RandomQuotes\RandomQuotes();
+echo "<i>" .$rq->generateRandomQuotes(100) . '</i>';
 
-    $rq = new \RandomQuotes\RandomQuotes();
-    echo $rq->generateRandomQuotes(100);
+if(file_exists('media/facepalm.jpg')) {
+    $opentag = "<table><tr><td><img src='media/facepalm.jpg'></td><td>";
+    $closetag = "</td></tr></table>";
+} else {
+    $opentag = "";
+    $closetag = "";
+}
 
-    if(file_exists('env.php')) {
-        $env = include 'env.php';
-    } else {
-        exit('Not available');
-    }
+echo $opentag;
 
-	var_dump(getWeather($env));
+if(file_exists('env.php')) {
+    $env = include 'env.php';
+} else {
+    exit('Not available');
+}
 
-
-	function getWeather($env)
-	{
-		$apiKey = $env['api_key'];
-		$cityId = $env['api_city'];
-		$url = sprintf($env['api_url'], $cityId, $apiKey);
+$apiKey = $env['api_key'];
+$cityId = $env['api_city'];
+$url = sprintf($env['api_url'], $cityId, $apiKey);
 
 
-		$ch = curl_init();
+$ch = curl_init();
 
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-		curl_setopt($ch, CURLOPT_VERBOSE, 0);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		$response = curl_exec($ch);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_VERBOSE, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$response = curl_exec($ch);
 
-		curl_close($ch);
-		$data = json_decode($response);
-		$currentTime = time();
+curl_close($ch);
+$data = json_decode($response);
+$currentTime = time();
 
-		return $data;
-	}
+var_dump($data);
+echo $closetag;
+?>
+</body>
+</html>
