2a8c76c0eb706f62329627ff745f9e08704a7256
Dave DPI <dave@dpi.nl>
2020-07-28 15:23:59 +0200

    Settings rights.

--- release-link.sh
+++ release-link.sh
@@ -5,6 +5,7 @@ webroot=$2
 
 echo "Linking $releasename to $webroot"
 
+chmod 644 "/var/www/releases/$releasename/settings.php"
 ln -s /var/www/media "/var/www/releases/$releasename/media"
 ln -s /var/www/common/env.php "/var/www/releases/$releasename/env.php"
 rm $webroot
